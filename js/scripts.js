3972a7a986abd8c0c974f776467388db8134d20d
Erik Killops and Jim Padilla <erik@>
2016-10-18 15:07:50 -0700

    Complete jquery for span inputs

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,17 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var inputPerson1 = $("input#person1").val();
     var inputPerson2 = $("input#person2").val();
+    var inputAnimal = $("input#animal").val();
+    var inputExclamation = $("input#exclamation").val();
+    var inputVerb = $("input#verb").val();
+    var inputNoun = $("input#noun").val();
 
     $(".person1").text(inputPerson1);
     $(".person2").text(inputPerson2);
-    $(".animal").text("something");
-    $(".exclamation").text("something else");
-    $(".verb").text("something");
-    $(".noun").text("something else");
+    $(".animal").text(inputAnimal);
+    $(".exclamation").text(inputExclamation);
+    $(".verb").text(inputVerb);
+    $(".noun").text(inputNoun);
 
     $("#story").show();
 
