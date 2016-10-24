ed3a610623cac2fcedc6dab347eed8c4c06e4f0d
Austin Blain and Jim Padilla <jim@>
2016-10-24 09:32:47 -0700

    consolidate JS in 1 function

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,15 @@
 $(document).ready(function() {
 
   $("#blanks form").submit(function(event) {
-    var inputPerson1 = $("input#person1").val();
-    var inputPerson2 = $("input#person2").val();
-    var inputAnimal = $("input#animal").val();
-    var inputExclamation = $("input#exclamation").val();
-    var inputVerb = $("input#verb").val();
-    var inputNoun = $("input#noun").val();
+    var blankIds = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    $(".person1").text(inputPerson1);
-    $(".person2").text(inputPerson2);
-    $(".animal").text(inputAnimal);
-    $(".exclamation").text(inputExclamation);
-    $(".verb").text(inputVerb);
-    $(".noun").text(inputNoun);
+    blankIds.forEach(function(blankId) {
+      var userInput = $("input#" + blankId).val();
+      $("." + blankId).text(userInput);
+    });
 
     $("#story").show();
 
     event.preventDefault();
-  });
-
-
+    });
 });
